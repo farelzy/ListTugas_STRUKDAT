5dae4659fe8ad101eaeae02b871bf9b6c881776c
IzazRz <73918830+farizaw123@users.noreply.github.com>
2024-12-16 12:23:42 +0700

    Base

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <sstream>
+#include <iomanip>
+
+using namespace std;
+
+struct Tugas {
+    string tugasID;
+    string namaTugas;
+    string mataKuliah;
+    string deskripsi;
+    string deadline;
+};
