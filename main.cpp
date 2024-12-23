0a29dc9105ac29c58f20f85b3396b45ba1dc6d9b
IzazRz <73918830+farizaw123@users.noreply.github.com>
2024-12-23 16:21:51 +0700

    Base edit

--- main.cpp
+++ main.cpp
@@ -1,16 +1,34 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <ctime>
-#include <sstream>
-#include <iomanip>
-
+#include <algorithm>
 using namespace std;
 
-struct Tugas {
-    string tugasID;
-    string namaTugas;
-    string mataKuliah;
-    string deskripsi;
+struct Task {
+    int taskID;
+    string taskName;
+    string course;
+    string description;
     string deadline;
+    bool isCompleted;
+
+    Task(int id, string name, string course, string desc, string dead)
+        : taskID(id), taskName(name), course(course), description(desc), deadline(dead), isCompleted(false) {}
 };
+
+void addTask(vector<Task> &taskList, int &taskCount) {
+    string taskName, course, description, deadline;
+
+    cout << "\nMasukkan nama tugas: ";
+    getline(cin, taskName);
+    cout << "Masukkan nama mata kuliah: ";
+    getline(cin, course);
+    cout << "Masukkan deskripsi tugas: ";
+    getline(cin, description);
+    cout << "Masukkan deadline (format: yyyy-mm-dd): ";
+    getline(cin, deadline);
+
+    taskCount++;
+    taskList.emplace_back(taskCount, taskName, course, description, deadline);
+    cout << "Tugas ID " << taskCount << " telah ditambahkan." << endl;
+}
